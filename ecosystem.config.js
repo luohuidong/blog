b850290eb6b0f1499399f34a7cf9e6611c57066d
luohuidong <luohuidong01@126.com>
2018-09-09 19:38:04 +0800

    modified pm2 config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
         NODE_ENV: "production",
       },
       env: {
-        PM2_SERVE_PATH: ".",
+        PM2_SERVE_PATH: "./public",
         PM2_SERVE_PORT: 8001,
       }
     }]

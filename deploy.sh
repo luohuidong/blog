22333aba477bf765965046662d23f67d2742a81a
luohuidong <luohuidong01@126.com>
2018-12-23 23:12:30 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 hexo generate
-rm -rf /www/wwwroot/blog_luohuidong_cn/*
-cp -r public/* /www/wwwroot/blog_luohuidong_cn/
+
+if [ $? -eq 0 ] 
+    then 
+        echo '博客静态文件生成成功'
+        rm -rf /www/wwwroot/blog_luohuidong_cn/*
+else
+    echo 'hexo 未安装'
+    exit
+fi
+
+
+if [ $? -eq 0 ]
+    then
+        echo '清除旧静态文件成功'
+        cp -r public/* /www/wwwroot/blog_luohuidong_cn/
+else 
+    echo '清除旧文件失败'
+    exit
+fi
+
+
+if [ $? -eq 0 ]
+    then
+        echo '复制新静态文件成功'
+else
+    echo '复制新静态文件失败'
+    exit
+fi
